788eec76f9cb02883416029c03524b32e0940208
Federico Magnani <magnani.federico@me.com>
2025-11-25 00:47:09 +0100

    sofficini

--- src/lib/database/bites.ts
+++ src/lib/database/bites.ts
@@ -7,5 +7,6 @@ export const foods: MenuEntry[] = [
     new MenuEntry("4 pizze quadrate", 420, "Scaldate al forno, leggerissime", 'pizzette.jpg'),
     new MenuEntry("Anelli di formaggio", 390, "Non credo serva descrizione", 'anelli-formaggio.webp'),
     new MenuEntry("Sofficini bacon e patate", 340, "Gusto americato, super morbidi", 'sofficini-americani.jpg'),
+    new MenuEntry("Sofficini pomodoro e mozzarella", 250, "Un grande classico al forno", 'sofficini-pomodoro.jpg'),
     new MenuEntry("Nuggets di cavolfiore", 450, "Nuggets croccantissimi vegetali", 'nuggets-cavolfiore.jpg'),
 ];
