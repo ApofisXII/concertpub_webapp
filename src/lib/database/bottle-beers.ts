b37eb333eda3c258449b05fd97c8de33e6d4c3ec
Federico Magnani <magnani.federico@me.com>
2025-11-24 11:21:15 +0100

    baladins

--- src/lib/database/bottle-beers.ts
+++ src/lib/database/bottle-beers.ts
@@ -2,7 +2,7 @@ import { MenuEntry } from "$lib/models/menu-entry.ts";
 
 export const foods: MenuEntry[] = [
     new MenuEntry("Nazionale Bianca - Baladin", 270, "Italian blanche • 5% vol • 330ml", 'baladin-bianca.jpg'),
-    new MenuEntry("Nazionale Ambrata - Baladin", 270, "Italian IPA • 5,5% vol • 330ml", 'baladin-ambrata.jpg'),
+    new MenuEntry("Nazionale Ambrata - Baladin", 270, "Italian IPA • 5,5% vol • 330ml", 'baladin-ambrata.webp'),
     new MenuEntry("Complot", 220, "Mediterranean IPA • 6,6% vol • 330ml", 'complot-ipa.png'),
     new MenuEntry("Guinness Draught Stout", 150, "Irish stout • 4,2% vol • 330ml", 'guinness.jpg'),
     new MenuEntry("Urquel Pils", 130, "PILS • 4,4% vol • 330ml", 'urquel.png'),
