33f84c4c9ccf2db6635fdc3a7d507cb2a9264dfb
Federico Magnani <magnani.federico@me.com>
2025-12-12 19:53:28 +0100

    menu adjust

--- src/lib/database/bites.ts
+++ src/lib/database/bites.ts
@@ -4,9 +4,6 @@ export const foods: MenuEntry[] = [
     new MenuEntry("Nachos con formaggio", 350, "Serviti con salse, fatti al momento", 'nachos.webp'),
     new MenuEntry("Anelli di cipolla", 350, "Pastellati alla birra", 'onion-rings.webp'),
     new MenuEntry("Patatine fritte Twisters", 370, "Fatte al forno, con numerose salse", 'twisters.webp'),
-    new MenuEntry("4 pizze quadrate", 420, "Scaldate al forno, leggerissime", 'pizzette.jpg'),
     new MenuEntry("Anelli di formaggio", 390, "Non credo serva descrizione", 'anelli-formaggio.webp'),
-    new MenuEntry("Sofficini bacon e patate", 340, "Gusto americato, super morbidi", 'sofficini-americani.jpg'),
     new MenuEntry("Sofficini pomodoro e mozzarella", 250, "Un grande classico al forno", 'sofficini-pomodoro.jpg'),
-    new MenuEntry("Nuggets di cavolfiore", 450, "Nuggets croccantissimi vegetali", 'nuggets-cavolfiore.jpg'),
 ];
