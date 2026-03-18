e1d8b78dac9ea180ed27e9fbb3d00c9079b32122
Federico Magnani <magnani.federico@me.com>
2026-03-18 01:07:43 +0100

    adeguamento prezzi

--- src/lib/database/bites.ts
+++ src/lib/database/bites.ts
@@ -2,8 +2,7 @@ import { MenuEntry } from "$lib/models/menu-entry.ts";
 
 export const foods: MenuEntry[] = [
     new MenuEntry("Nachos con formaggio", 350, "Serviti con salse, fatti al momento", 'nachos.webp'),
-    new MenuEntry("Anelli di cipolla", 350, "Pastellati alla birra", 'onion-rings.webp'),
-    new MenuEntry("Patatine fritte Twisters", 370, "Fatte al forno, con numerose salse", 'twisters.webp'),
-    new MenuEntry("Anelli di formaggio", 390, "Non credo serva descrizione", 'anelli-formaggio.webp'),
-    new MenuEntry("Sofficini pomodoro e mozzarella", 250, "Un grande classico al forno", 'sofficini-pomodoro.jpg'),
+    new MenuEntry("Anelli di cipolla", 380, "Pastellati alla birra", 'onion-rings.webp'),
+    new MenuEntry("Patatine fritte Twisters", 380, "Fatte al forno, con numerose salse", 'twisters.webp'),
+    new MenuEntry("Sofficini pomodoro e mozzarella", 300, "Un grande classico al forno", 'sofficini-pomodoro.jpg'),
 ];
